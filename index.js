2769165868a10e07826da057c6f7a082f2efc877
Mpango4 <118990291+Mpango4@users.noreply.github.com>
2023-11-15 16:21:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+function show() {
+    var display = document.getElementById("display");
+    if (display.style.display === "none") {
+        display.style.display = "block";
+    } else {
+        display.style.display = "none";
+    }
+}
+function display() {
+    var form = document.getElementById("form");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+
+function skill() {
+    var Skills = document.getElementById("Skills");
+    if (Skills.style.display === "none") {
+        Skills.style.display = "block";
+    } else {
+        Skills.style.display = "none";
+    }
+}
+
+function hideSkills() {
+    var Skills = document.getElementById("Skills");
+    if (Skills.style.display === "block") {
+        Skills.style.display = "none";
+    }
+}
+
+function experience() {
+    var experienceDetail = document.getElementById("experiences");
+    if (experienceDetail.style.display === "none") {
+        experienceDetail.style.display = "block";
+    } else {
+        experienceDetail.style.display = "none";
+    }
+}
+
+function hideExperience() {
+    var experienceDetail = document.getElementById("experiences");
+    if (experienceDetail.style.display === "block") {
+        experienceDetail.style.display = "none";
+    } 
+}
+
+
+function education() {
+    var educationDetail = document.getElementById("educations");
+    if (educationDetail.style.display === "none") {
+        educationDetail.style.display = "block";
+    } else {
+        educationDetail.style.display = "none";
+    }
+}
+function hideEducation() {
+    var educationDetail = document.getElementById("educations");
+    if (educationDetail.style.display === "block") {
+        educationDetail.style.display = "none";
+    } 
+}
+function project() {
+    var projectDetail = document.getElementById("projects");
+    if (projectDetail.style.display === "none") {
+        projectDetail.style.display = "block";
+    } else {
+        projectDetail.style.display = "none";
+    }
+}
+function hide() {
+    var projectDetail = document.getElementById("projects");
+    if (projectDetail.style.display === "block") {
+        projectDetail.style.display = "none";
+    } 
+}
